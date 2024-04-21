d3956ea52802a430a55633139b7a70dbb1e7775d
HACKER <chinmay.kanashetti@chatbot.team>
2024-04-21 21:46:50 +0530

    changesDone

--- controller/signUpController.js
+++ controller/signUpController.js
@@ -6,13 +6,19 @@ exports.userSignUp = async (req, res) => {
             email_mobile_number: mobileNumberOrEmail,
             password: password
         }
-        let insertSignupData = new signUpModel(reqObj);
-        let saveSignUpData = await insertSignupData.save();
-        if (saveSignUpData) {
-            res.status(200).json({ signUp: true, message: "Signed Up Sucessfully" });
+        let userExist = await signUpModel.findOne({ email_mobile_number: mobileNumberOrEmail });
+        if (userExist) {
+            res.status(200).json({ signUp: false, message: "Account with this email or mobile number already exists" });
         }
         else {
-            res.status(500).json({ signUp: false, message: "We’re currently processing too many requests. Please try again later" });
+            let insertSignupData = new signUpModel(reqObj);
+            let saveSignUpData = await insertSignupData.save();
+            if (saveSignUpData) {
+                res.status(200).json({ signUp: true, message: "Signed Up Sucessfully" });
+            }
+            else {
+                res.status(204).json({ signUp: false, message: "We’re currently processing too many requests. Please try again later" });
+            }
         }
     } catch (error) {
         res.status(500).json({ signUp: false, message: "We’re currently processing too many requests. Please try again later" });
